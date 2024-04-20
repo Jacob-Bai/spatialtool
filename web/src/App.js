f64fb5b41fcf1ec7a92d447a56fc571894f9eae3
Jacob-Bai <jacob.bai.au@gmail.com>
2024-04-20 11:50:42 +0100

    Major updates to frontend: 1. move to react bootstrap 2. move upload request to xmlhttp 3. add download request 4. add delete request

--- web/src/App.js
+++ web/src/App.js
@@ -1,22 +1,28 @@
-import { Dropzone, FileMosaic } from "@files-ui/react";
 import { useCookies } from 'react-cookie';
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
+import { Form, Button, ProgressBar } from 'react-bootstrap';
 
-
-const API_URL = "/v1";
-
-function handleDownload() { }
-function handleDelete() { }
+const API_GET_ID = "/v1/id";
+const API_GET_STATUS = "/v1/status?id="
+const API_POST_VIDEO = "/v1/video/";
+const API_GET_VIDEO = "/v1/video?id=";
+const API_DEL_VIDEO = "/v1/video/";
+const supportFormats = ['.avi', '.mp4', '.mov'];
+const maxFileSize = 500*1024*1024;
 
 export default function MainPage() {
 
-    let sessionStatus = "empty";
-    const [cookies, setCookie] = useCookies(['sessionId']);
-    const [extFiles, setExtFiles] = useState([]);
+    let sessionStatus = "new";
+    const [cookies, setCookie, removeCookie] = useCookies(['sessionId']);
+    const [uploadFile, setUploadFile] = useState({});
+    const [fileValid, setFileValid] = useState(false);
+    const [fileError, setFileError] = useState("");
+    const [jobProgress, setJobProgress] = useState(0);
+    const [inProgress, setInProgress] = useState(false);
 
     const getSessionId = async () => {
         try {
-            const res = await fetch(API_URL + '/id');
+            const res = await fetch(API_GET_ID);
             if (!res.ok) {
                 throw new Error('Getting new id failed');
             }
@@ -27,9 +33,10 @@ export default function MainPage() {
             console.error(err);
         }
     }
+
     const getSessionStatus = async () => {
         try {
-            const res = await fetch(API_URL + '/status?id=' + cookies.sessionId);
+            const res = await fetch(API_GET_STATUS + cookies.sessionId);
             if (!res.ok) {
                 throw new Error('Getting status failed');
             }
@@ -41,15 +48,59 @@ export default function MainPage() {
             console.error(err);
         }
     }
-    const updateFile = (files) => {
-        setExtFiles(files);
+
+    const updateFile = (e) => {
+        setUploadFile({});
+        setFileValid(false);
+        if (e.target.files.length === 0) {
+            setFileError("");
+        } else if (e.target.files[0].size > maxFileSize) {
+            setFileError("Please choose a file with smaller size");
+        } else if (!supportFormats.includes(e.target.files[0].type.replace("video/","."))) {
+            setFileError("Please choose a file with supported format")
+        } else {
+            setUploadFile(e.target.files[0]);
+            setFileValid(true);
+        }
     };
-    const handleStart = async (filesToUpload) => {
-        console.log("advanced demo start upload id:", cookies.sessionId);
-    };
-    const handleFinish = (uploadedFiles) => {
-        console.log("advanced demo finish upload", uploadedFiles);
-    };
+
+    const handleUpload = async () => {
+        if (!fileValid) {
+            return;
+        }
+        setInProgress(true);
+
+        const formData = new FormData();
+        formData.append(cookies.sessionId, uploadFile);
+
+        let xhr = new XMLHttpRequest();
+        xhr.upload.addEventListener("progress", ({loaded, total}) => {
+            const progress = Math.floor(loaded / total * 100);
+            setJobProgress(progress);
+        });
+        xhr.addEventListener("load", () => {
+            setInProgress(false);
+
+        });
+        xhr.addEventListener("error", () => {
+            throw new Error("upload failed");
+        });
+        xhr.open("POST", API_POST_VIDEO + cookies.sessionId); 
+        xhr.send(formData);
+    }
+
+    const handleDelete = async () => {
+        try {
+            const res = await fetch(API_DEL_VIDEO + cookies.sessionId, { method: 'DELETE' });
+            if (!res.ok) {
+                throw new Error('Video deletion failed');
+            }
+            removeCookie('sessionId', { path: '/' });
+        }
+        catch (err) {
+            console.error(err);
+        }
+    }
 
     if (cookies.sessionId) {
         getSessionStatus();
@@ -59,40 +110,40 @@ export default function MainPage() {
 
     return (
         <section className="main-sec">
+            <div className='top-bar'>LOGO</div>
             <div className="container">
                 <h1 className="heading">Spatial Tool</h1>
                 <p className="description">Convert Side-By-Side 3D video to Spatial video</p>
-                <div className="dropbox">
-                    <Dropzone minHeight="210px" maxFiles={1} maxFileSize={500 * 1024 * 1024}
-                        onChange={updateFile} value={extFiles} accept=".avi,.mp4,.mov"
-                        label="Drop file here or click to browse"
-                        uploadConfig={{
-                            url: API_URL + "/video/" + cookies.sessionId,
-                            uploadLabel: cookies.sessionId,
-                            cleanOnUpload: true,
-                            uploadingMessage: "uploading test"
-                        }}
-                        footerConfig={{uploadProgressMessage: false}}
-                        onUploadStart={handleStart}
-                        onUploadFinish={handleFinish}
-                        actionButtons={{
-                            position: "after",
-                            uploadButton: { resetStyles: true, className: "upload-button" }
-                        }}
-                        behaviour="replace" >
-                        {extFiles.map((file) => (
-                            <FileMosaic {...file} />
-                        ))}
-                    </Dropzone>
+                <div className='anime-zone'>
+                    <Form.Group className={ !inProgress? 'dropbox':'hide' }>
+                        <Form.Label>
+                            Maximum size: 500 MB<br/>
+                            {"Support formats: " + supportFormats.join(',')}
+                        </Form.Label>
+                        <Form.Control type="file" onChange={updateFile}  isInvalid={!fileValid}/>
+                        <Form.Control.Feedback type="invalid"> {fileError} </Form.Control.Feedback>
+                    </Form.Group>
+                    <div className={ inProgress? 'job-status':'hide' }>
+                        {sessionStatus}
+                        <ProgressBar animated now={jobProgress} />
+                    </div>
                 </div>
-                <button className="button" onClick={handleDownload}>Download</button>
-                <button className="button" onClick={handleDelete}>Delete</button>
+                <Button className="button" variant="primary" onClick={handleUpload} disabled={!fileValid}>
+                    Upload
+                </Button>
+                <Button href={API_GET_VIDEO + cookies.sessionId} className="button" variant="success" disabled={!fileValid}>
+                    Download
+                </Button>
+                <Button className="button" variant="danger" onClick={handleDelete} disabled={!fileValid}>
+                    Delete
+                </Button>
                 <div className="note">
                     <p id="line">How to use this tool:</p>
                     <p id="line">1. Choose the type of source video.</p>
                     <p id="line">2. Upload the video file.</p>
-                    <p id="line">3. Download or delete the converted video.</p>
-                    <p id="line">4. Let's convert another one!</p>
+                    <p id="line">3. Download the converted video.</p>
+                    <p id="line">4. Delete the converted video.</p>
+                    <p id="line">5. Let's convert another one!</p>
                 </div>
             </div>
         </section>
